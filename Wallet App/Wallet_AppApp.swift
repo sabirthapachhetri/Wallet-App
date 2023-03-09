a8719e9da8de8da0551310655ecae7492a11f5cf
Sabir <sabirthapa19@gmail.com>
2023-03-09 13:38:49 +0545

    Initial Commit

---
+++ Wallet App/Wallet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_AppApp.swift
+//  Wallet App
+//
+//  Created by Sabir's MacBook on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
